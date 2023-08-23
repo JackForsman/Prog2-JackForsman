f7c3fdb94a08aa19f3c58949f4bcafab7aa8d704
JackForsman <jack.forsman@skola.taby.se>
2023-08-23 10:43:24 +0200

    Uppgifter

---
+++ numeriska datatyper/1.py
@@ -0,0 +1,7 @@
+import math
+def Googol():
+    googol_timmar = (10**100)/60
+    print(googol_timmar%24)
+
+
+Googol()
---
+++ numeriska datatyper/3.py
@@ -0,0 +1,4 @@
+import math
+complex = complex(1,1)
+tal = 2**(complex)
+print(tal)
