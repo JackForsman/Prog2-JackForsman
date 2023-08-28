612cd806819cd07db7875a596844851a4563cd9f
JackForsman <jack.forsman@skola.taby.se>
2023-08-28 15:07:10 +0200

    Rövarspråk

---
+++ elementära datatyper/uppgifter.py
@@ -0,0 +1,16 @@
+def rövarspråk():
+    vokaler = ["a","e","i","o","u","y","å","ä","ö"]
+    ord = input("Skriv in ditt ord på svenska:")
+    rövarord = []
+    for bokstav in ord:
+        if bokstav != " " and bokstav.lower() not in vokaler:
+            rövarord.append(bokstav + "o" + bokstav)
+        else:
+            rövarord.append(bokstav)
+    rövarord = "".join(rövarord)
+    print(rövarord)
+
+rövarspråk()
+
+
+
