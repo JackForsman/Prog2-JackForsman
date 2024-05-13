db715d93a2c94f6c77482237fff70883141b0a65
JackForsman <116351259+JackForsman@users.noreply.github.com>
2024-05-13 12:56:31 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const http = require("http").Server(app)
-const io = require("socket.io")(http)
 const port = 3000;
+const cors = require('cors');
 
+app.use(express.json());
+
+app.use(cors({
+    origin: "*"
+    })
+)
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'phrases_db'
+    database: 'post_db'
 });
 
 connection.connect((err) => {
@@ -22,23 +27,55 @@ connection.connect((err) => {
 });
 
 app.get('/post_data', (req, res) => {
-    const sql = "SELECT * FROM phrases";
+    const sql = "SELECT * FROM posts";
     connection.query(sql, (err, result) => {
         if (err) {
-            console.error('Error retrieving phrases:', err);
-            res.status(500).send('Error retrieving phrases');
+            console.error('Error retrieving posts:', err);
+            res.status(500).send('Error retrieving posts');
             return;
         }
         res.json(result);
     });
 });
 
-socket.on("/make_post", function (data) {
-    console.log(data)
-  })
+app.post('/make_post', (req, res) => {
+    const post_title = req.body.title
+    const post_author = req.body.author
+    const post_content = req.body.content
+    const post_date = req.body.date
+    const sql = "INSERT INTO posts (Title, Author, Content, Date) VALUES (?,?,?,?)"
+    connection.query(sql, [post_title,post_author,post_content, post_date], (err, result) => {
+        if (err) {
+            console.error('Error making post:', err);
+            res.status(500).send('Error making post');
+            return;
+        }
+        res.status(200).send("Successfull");
+        console.log("added")
+    });
+    
+});
 
 
-// Start Server
+app.get('/post/:postId', (req, res) => {
+    const sql = "SELECT * FROM posts WHERE Id = ?";
+    connection.query(sql, [req.params.postId], (err, result) => {
+        if (err) {
+            console.error('Error fetching post:', err);
+            res.status(500).send('Error fetching post');
+            return;
+        }
+        if (result.length === 0) {
+            res.status(404).send('Post not found');
+            return;
+        }
+        console.log(result)
+        res.json(result)
+    });
+});
+
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
