949870f145f6dfb43663531715fd10555b5d8e62
JackForsman <116351259+JackForsman@users.noreply.github.com>
2024-05-20 13:08:04 +0200

    Add files via upload

---
+++ slutprojekt/post_db.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 20 maj 2024 kl 12:53
+-- Serverversion: 10.4.32-MariaDB
+-- PHP-version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `post_db`
+--
+CREATE DATABASE `post_db`;
+USE `post_db`;
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `comments`
+--
+
+CREATE TABLE `comments` (
+  `Id` int(100) NOT NULL,
+  `postId` int(100) NOT NULL,
+  `Author` varchar(100) NOT NULL,
+  `Content` varchar(10) NOT NULL,
+  `Likes` int(240) NOT NULL DEFAULT 0,
+  `Date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `posts`
+--
+
+CREATE TABLE `posts` (
+  `Id` int(100) NOT NULL,
+  `Title` varchar(1000) NOT NULL,
+  `Author` varchar(100) NOT NULL,
+  `Content` varchar(100) NOT NULL,
+  `Likes` int(100) NOT NULL DEFAULT 0,
+  `Date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Index för tabell `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT för tabell `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+ALTER TABLE `posts` AUTO_INCREMENT=10001;
+ALTER TABLE `posts` AUTO_INCREMENT=1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
